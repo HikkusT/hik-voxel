a9df0c56e7b45150d8739c34f9244477503ce56b
Hik <luizhenrique98@gmail.com>
2024-08-11 22:33:57 -0300

    fix: add cleanup code for window

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,35 +1,38 @@
-//#include <fmt/core.h>
 #include <SDL.h>
 #include <spdlog/spdlog.h>
 #include "Window.h"
 
 namespace cubik {
-    Window::Window(glm::ivec2 size, const std::string& name) {
-      if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        spdlog::error("Failed to initialize SDL: {}", SDL_GetError());
-        abort();
-      }
-
-      SDL_Window* window = SDL_CreateWindow(
-              name.c_str(),
-              SDL_WINDOWPOS_UNDEFINED,
-              SDL_WINDOWPOS_UNDEFINED,
-              size.x,
-              size.y,
-              SDL_WINDOW_VULKAN);
-      if (window == nullptr) {
-        spdlog::error("Failed to create SDL window: {}", SDL_GetError());
-        abort();
-      }
+  Window::Window(glm::ivec2 size, const std::string &name) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      spdlog::error("Failed to initialize SDL: {}", SDL_GetError());
+      abort();
     }
 
-    void Window::processInputs() {
-      SDL_Event e;
-      while (SDL_PollEvent(&e) != 0) {
-        if (e.type == SDL_QUIT)
-          _isClosed = true;
-
-        // TODO: Handle minimize/maximize
-      }
+    _window = SDL_CreateWindow(
+        name.c_str(),
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        size.x,
+        size.y,
+        SDL_WINDOW_VULKAN);
+    if (_window == nullptr) {
+      spdlog::error("Failed to create SDL window: {}", SDL_GetError());
+      abort();
     }
+  }
+
+  void Window::processInputs() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT)
+        _isClosed = true;
+
+      // TODO: Handle minimize/maximize
+    }
+  }
+
+  Window::~Window() {
+    SDL_DestroyWindow(_window);
+  }
 }
--- src/Window.h
+++ src/Window.h
@@ -1,16 +1,19 @@
 #pragma once
 
+#include <SDL.h>
 #include <string>
 #include <glm/vec2.hpp>
 
 namespace cubik {
   class Window {
   private:
-    bool _isClosed = false;
+    struct SDL_Window* _window { nullptr };
+    bool _isClosed { false };
   public:
     Window(glm::ivec2 size, const std::string &name);
+    ~Window();
 
-    bool IsClosed() { return _isClosed; }
+    bool IsClosed() const { return _isClosed; }
 
     void processInputs();
   };
